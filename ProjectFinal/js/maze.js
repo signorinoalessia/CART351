5d209a762435777749ec6fd7075a811faacc915b
Alessia Signorino <a_sign@cda024ev5709x.local>
2018-11-07 14:38:07 -0500

    issues with making canvas full screen, fixed size for now

--- ProjectFinal/js/maze.js
+++ ProjectFinal/js/maze.js
@@ -6,27 +6,30 @@ let CANVAS_WIDTH = 800;
 let CANVAS_HEIGHT = 600;
 
 function setup() {
-  let myCanvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT,WEBGL);
+  let myCanvas = createCanvas(800,600,WEBGL);
   // camera's field of view and depth
   let fov = 60/180*PI;
   let cameraZ = CANVAS_HEIGHT/2.0/tan(fov/2.0);
   // perspective(frustum [fovy], [aspect], [near plane], [far plane])
   perspective(60/180*PI, CANVAS_WIDTH/CANVAS_HEIGHT, cameraZ*0.1, cameraZ*10)
-  myCanvas.position(x,y);
-  myCanvas.parent('mazeContainer');
-  background(255,0,200);
+
+  // let x = (windowWidth - width) / 2;
+  // let y = (windowHeight - height) / 2;
+  // myCanvas.position(x,y);
+  // myCanvas.parent('mazeContainer');
+  // background(255,0,200);
 }
 
 function draw() {
   background(100);
   // Orbit control allows the camera to orbit around a target.
   orbitControl();
-  // nested for loop for x and y coords
+  // nested for loop for x and y coords (GRID OF BOXES)
   for (let i=-1;i<2;i++) {
     for (let j=-2;j<3;j++) {
       push();
       translate(i*160,0,j*160);
-      box(40,40,40);
+      box(40);
       pop();
     }
   }
