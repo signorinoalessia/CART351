06b7a8adab96cd37139e57cbef8e0d2d425bf7a5
COHDS Admin <cohdsadmin@2010-imac1-2.concordia.ca>
2018-11-30 15:55:51 -0500

    updateDB small changes

--- ProjectFinal/php/updateDB.php
+++ ProjectFinal/php/updateDB.php
@@ -4,7 +4,7 @@ class MyDB extends SQLite3
    {
       function __construct()
       {
-         $this->open('../db/graffitiGallery.db');
+         $this->open('../db/users.db');
       }
    }
 try
@@ -17,4 +17,12 @@ catch(Exception $e)
 }
 
 // http://clab.concordia.ca/intro-to-sql-lite/
+
+$sql_update="UPDATE usersTable SET activeCell = 'Untitled' WHERE username='$user_es'";
+   // again we do error checking when we try to execute our SQL statements on the db
+   $ok1 = $db ->exec($sql_update);
+   if (!$ok1) die("Cannot execute statement.");
+   // if we reach this point then all the data has been updated successfully.
+   echo "UPDATE OF table called artCollection successful";
+
 ?>
